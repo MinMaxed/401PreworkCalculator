2020afa3d2626fa231e431bf861ce369eb094539
Max <gusstorm132@gmail.com>
2018-06-03 16:08:15 -0700

    Prework simple calculator

---
+++ PreWorkCalculator/PreWorkCalculator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //store result
+            double result = 0;
+
+            //Obtain First Number
+            Console.WriteLine("First Number:");
+            double firstNum = Convert.ToDouble(Console.ReadLine());
+
+            //Obtain Second Number
+            Console.WriteLine("Second Number:");
+            double secondNum = Convert.ToDouble(Console.ReadLine());
+
+            // Operation to perform on the numbers
+            Console.WriteLine("Operation Type +, -, *, /, ^ (exponent)");
+            string stringOperationType = Console.ReadLine();
+
+            // Calculations based on Operation choice
+            if (stringOperationType == "+")
+            {
+                result = firstNum + secondNum;
+            }
+            else if (stringOperationType == "-")
+            {
+                result = firstNum - secondNum;
+            }
+            else if (stringOperationType == "*")
+            {
+                result = firstNum * secondNum;
+            }
+            else if (stringOperationType == "/")
+            {
+                result = firstNum / secondNum;
+            }
+            else if (stringOperationType == "^")
+            {
+                result = Math.Pow(firstNum, secondNum);
+            }
+
+            //display solution. Future Work: Want to be able to continue to use results
+            Console.WriteLine("\nResult of " + firstNum + " " + stringOperationType + " " + secondNum + " = " + result);
+            Console.ReadKey();
+        }
+    }
+}
